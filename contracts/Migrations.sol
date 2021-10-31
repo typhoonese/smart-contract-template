f524f1dfefdbc8f510df412de997caa377875bf9
nufyatese <nufyatese@outlook.com>
2021-10-31 09:48:05 +0000

    Fix linting errors in Migrations.sol

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity ^0.8.9;
 
 contract Migrations {
   address public owner = msg.sender;
-  uint public last_completed_migration;
+  uint public lastCompletedMigration;
 
   modifier restricted() {
-    require(
+      require(
       msg.sender == owner,
       "This function is restricted to the contract's owner"
     );
@@ -14,6 +14,6 @@ contract Migrations {
   }
 
   function setCompleted(uint completed) public restricted {
-    last_completed_migration = completed;
+    lastCompletedMigration = completed;
   }
 }

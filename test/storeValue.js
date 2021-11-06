6abd41c6cf458347e4ab9c8a2abe3fd0704cb9b9
typhoonese <typhoonese@outlook.com>
2021-11-06 23:56:50 +0000

    Add test case for setValue fn: 1/1 passing tests

--- test/storeValue.js
+++ test/storeValue.js
@@ -1,5 +1,14 @@
+//contracts
 const storageDemo = artifacts.require('StorageDemo');
+
+//test utils
 const truffleAssert = require('truffle-assertions');
+const BN = web3.utils.toBN;
+
+//test constants
+const valueSetEvent = 'ValueSet';
+const storageValue = 5;
+const expectedStorageValue = new BN(storageValue);
 
 contract(storageDemo, (accounts) => {
   let storageDemoInstance;
@@ -7,14 +16,14 @@ contract(storageDemo, (accounts) => {
     storageDemoInstance = await storageDemo.new();
   });
 
-  describe('Set value'),
-    () => {
-      it('Set value to 5: Assertion with event'),
-        async () => {
-          let tx = await storageDemoInstance.setValue(5, { from: accounts[0] });
-          truffleAssert.eventEmitted(tx, 'ValueSet', (ev) => {
-            return ev._from === accounts[0] && ev._value === +value;
-          });
-        };
-    };
+  it('When value is set to ' + storageValue + ',' + valueSetEvent + ' event should be emitted.', async () => {
+    let tx = await storageDemoInstance.setValue(storageValue, { from: accounts[0] });
+    expectedEventOutput = { _from: accounts[0], _value: expectedStorageValue };
+    truffleAssert.eventEmitted(
+      tx,
+      valueSetEvent,
+      expectedEventOutput,
+      'Event output should match ' + JSON.stringify(expectedEventOutput, null, 2),
+    );
+  });
 });

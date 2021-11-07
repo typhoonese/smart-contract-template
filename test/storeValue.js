ab5b53ba701e5210ea07aa951facf515f5d50094
typhoonese <typhoonese@outlook.com>
2021-11-07 00:01:54 +0000

    Add describe:Setting a storage value

--- test/storeValue.js
+++ test/storeValue.js
@@ -16,14 +16,16 @@ contract(storageDemo, (accounts) => {
     storageDemoInstance = await storageDemo.new();
   });
 
-  it('When value is set to ' + storageValue + ',' + valueSetEvent + ' event should be emitted.', async () => {
-    let tx = await storageDemoInstance.setValue(storageValue, { from: accounts[0] });
-    expectedEventOutput = { _from: accounts[0], _value: expectedStorageValue };
-    truffleAssert.eventEmitted(
-      tx,
-      valueSetEvent,
-      expectedEventOutput,
-      'Event output should match ' + JSON.stringify(expectedEventOutput, null, 2),
-    );
+  describe('Setting a storage value', async () => {
+    it('When value is set to ' + storageValue + ',' + valueSetEvent + ' event should be emitted.', async () => {
+      let tx = await storageDemoInstance.setValue(storageValue, { from: accounts[0] });
+      expectedEventOutput = { _from: accounts[0], _value: expectedStorageValue };
+      truffleAssert.eventEmitted(
+        tx,
+        valueSetEvent,
+        expectedEventOutput,
+        'Event output should match ' + JSON.stringify(expectedEventOutput, null, 2),
+      );
+    });
   });
 });

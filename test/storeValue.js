b9e279a4dbe68bb162921f84f8b97edaa6a98174
typhoonese <typhoonese@outlook.com>
2021-11-07 09:03:50 +0000

    Add cha

--- test/storeValue.js
+++ test/storeValue.js
@@ -3,6 +3,7 @@ const storageDemo = artifacts.require('StorageDemo');
 
 //test utils
 const truffleAssert = require('truffle-assertions');
+const { expect } = require('chai');
 const BN = web3.utils.toBN;
 
 //test constants
@@ -11,15 +12,15 @@ const storageValue = 5;
 const expectedStorageValue = new BN(storageValue);
 
 contract(storageDemo, (accounts) => {
-  let storageDemoInstance;
+  const expectedMsgSender = accounts[0];
   beforeEach(async () => {
-    storageDemoInstance = await storageDemo.new();
+    this.instance = await storageDemo.new();
   });
 
   describe('Setting a storage value', async () => {
     it('When value is set to ' + storageValue + ',' + valueSetEvent + ' event should be emitted.', async () => {
-      let tx = await storageDemoInstance.setValue(storageValue, { from: accounts[0] });
-      expectedEventOutput = { _from: accounts[0], _value: expectedStorageValue };
+      let tx = await this.instance.setValue(storageValue, { from: accounts[0] });
+      expectedEventOutput = { _from: expectedMsgSender, _value: expectedStorageValue };
       truffleAssert.eventEmitted(
         tx,
         valueSetEvent,

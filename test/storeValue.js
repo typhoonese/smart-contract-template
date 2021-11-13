af7b8d19af267de48428332d8b2a4f75105a4921
typhoonese <typhoonese@outlook.com>
2021-11-13 21:44:06 +0000

    Fix linting in storeValue.js

--- test/storeValue.js
+++ test/storeValue.js
@@ -17,30 +17,31 @@ const testMsg = 'On-chain value should match test data';
 
 contract(storageDemo, (accounts) => {
   const expectedMsgSender = accounts[0];
+  // eslint-disable-next-line mocha/no-top-level-hooks
   beforeEach(async () => {
     this.instance = await storageDemo.new();
   });
 
-  describe('Setting a storage value', async () => {
-    describe('truffleAssert.eventEmitted style 1', async () => {
-      it('When value is set to ' + storageValue + ',' + valueSetEvent + ' event should be emitted.', async () => {
-        let tx = await this.instance.setValue(storageValue, { from: accounts[0] });
-        expectedEventOutput = { _from: expectedMsgSender, _value: expectedStorageValue };
+  describe('Setting a storage value', () => {
+    describe('truffleAssert.eventEmitted style 1', () => {
+      it(`When value is set to ${storageValue}, ${valueSetEvent} event should be emitted.`, async () => {
+        const tx = await this.instance.setValue(storageValue, { from: accounts[0] });
+        const expectedEventOutput = { _from: expectedMsgSender, _value: expectedStorageValue };
         truffleAssert.eventEmitted(
           tx,
           valueSetEvent,
           expectedEventOutput,
-          'Event output should match ' + JSON.stringify(expectedEventOutput, null, 2),
+          `Event values should match expected event output`,
         );
       });
     });
   });
 
-  describe('Getting a storage value', async () => {
-    describe('Testing with chai', async () => {
-      it('When storage value is called with getValue(), the value should be ' + storageValue, async () => {
+  describe(`Getting a storage value`, () => {
+    describe(`Testing with chai`, () => {
+      it(`When storage value is called with getValue(), the value should be ${storageValue}`, async () => {
         await this.instance.setValue(storageValue, { from: accounts[0] });
-        let result = await this.instance.getValue({ from: accounts[0] });
+        const result = await this.instance.getValue({ from: accounts[0] });
         expect(result, testMsg).to.eq.BN(storageValue);
       });
     });
